6bd539e92755fd38a60cf79131ffb5cebd78f326
cbgduc <ducnguyenvlg@gmail.com>
2025-04-03 22:51:32 +0700

    use socket instead of request each 2 seconds

--- server.js
+++ server.js
@@ -1,18 +1,64 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const lib = require('./utils');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const port = 8080;
 
 app.use(bodyParser.json());
 
+// Key subscribers tracking
+const keySubscribers = {};
+
+// Socket.IO connection handling
+io.on('connection', (socket) => {
+    console.log('Client connected');
+
+    // Client subscribes to a key
+    socket.on('subscribe', async (key) => {
+        console.log(`Client subscribed to key: ${key}`);
+
+        // Add socket to a room named with the key
+        socket.join(key);
+
+        // Track subscriptions
+        if (!keySubscribers[key]) {
+            keySubscribers[key] = 0;
+        }
+        keySubscribers[key]++;
+
+        try {
+            // Send current value to the newly connected client
+            const value = await lib.view(key);
+            socket.emit('value_update', value);
+        } catch (err) {
+            socket.emit('value_update', 'No value found');
+        }
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+        // Simple disconnect handling
+    });
+});
+
+// Original routes
 app.post('/add', async (req, res) => {
     try {
         const { key, value } = req.body;
         await lib.write(key, value);
+
+        // Emit update to all subscribers of this key
+        if (keySubscribers[key]) {
+            io.to(key).emit('value_update', value);
+        }
+
         res.send("Insert a new record successfully!");
     } catch (err) {
         res.send(err.toString());
@@ -25,13 +71,11 @@ app.get('/get/:id', async (req, res) => {
         const value = await lib.view(id);
         res.status(200).send(value);
     } catch (err) {
-        res.send(err)
+        res.send(err);
     }
 });
 
-
 app.get('/viewer/:id', (req, res) => {
-    const id = req.params.id;
     res.sendFile(path.join(__dirname, "viewer.html"));
 });
 
@@ -39,6 +83,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'viewer.html'));
 });
 
-app.listen(port, () => {
+// Use server.listen instead of app.listen for Socket.IO
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
