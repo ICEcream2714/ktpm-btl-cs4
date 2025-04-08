afc16c914ee6fcd0461c24385c7ac84d8b6d9c59
Hoang Nguyen <ntchoang271@gmail.com>
2025-04-08 23:29:54 +0700

    some touchs

--- import_data.js
+++ import_data.js
@@ -3,42 +3,40 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const GoldPrice = require("./models/GoldPrice");
 const moment = require("moment");
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/cs4"
+const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/cs4";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(MONGO_URI);
-        console.log("MongoDB connected");
-    } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+    process.exit(1);
+  }
 };
-
 
 const importGoldPrices = async () => {
-    try {
-        const data = fs.readFileSync("c:/Users/hieu/Desktop/gold_prices.json", "utf-8");
-        const goldPrices = JSON.parse(`[${data.trim().replace(/\n/g, ",")}]`);
+  try {
+    const data = fs.readFileSync("gold_prices.json", "utf-8");
+    const goldPrices = JSON.parse(`[${data.trim().replace(/\n/g, ",")}]`);
 
-        const formattedGoldPrices = goldPrices.map((price) => ({
-            ...price,
-            timestamp: moment(price.timestamp, "DD/MM/YYYY HH:mm:ss").toDate(),
-        }));
+    const formattedGoldPrices = goldPrices.map((price) => ({
+      ...price,
+      timestamp: moment(price.timestamp, "DD/MM/YYYY HH:mm:ss").toDate(),
+    }));
 
-        await GoldPrice.insertMany(formattedGoldPrices);
-        console.log("Gold prices imported successfully");
-        process.exit();
-    } catch (error) {
-        console.error("Error importing gold prices:", error);
-        process.exit(1);
-    }
+    await GoldPrice.insertMany(formattedGoldPrices);
+    console.log("Gold prices imported successfully");
+    process.exit();
+  } catch (error) {
+    console.error("Error importing gold prices:", error);
+    process.exit(1);
+  }
 };
 
-
 const run = async () => {
-    await connectDB();
-    await importGoldPrices();
+  await connectDB();
+  await importGoldPrices();
 };
 
 run();
